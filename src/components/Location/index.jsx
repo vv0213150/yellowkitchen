8d8bc40880f3672386073ec64c7ac7fd56d77db0
Пользователь <vv0213159@gmail.com>
2024-12-12 19:47:33 +0600

    Fixed issues with iframe title and unused variables

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -3,8 +3,8 @@ import cls from './index.module.css'
 
 const Location = () => {
   return (
-    <div className={cls.location} >
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46793.75662760199!2d74.57606436953125!3d42.8599719!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7b57a0051d7%3A0xbb8c6d899e365958!2z0JDQstGA0L7RgNCw!5e0!3m2!1sru!2skg!4v1732958094095!5m2!1sru!2skg" width="1280px" height="600" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div className={cls.locationcn} >
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46793.75662760199!2d74.57606436953125!3d42.8599719!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7b57a0051d7%3A0xbb8c6d899e365958!2z0JDQstGA0L7RgNCw!5e0!3m2!1sru!2skg!4v1732958094095!5m2!1sru!2skg" width="1280px" height="600" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Google Map location"></iframe>
     </div>
   )
 }
--- src/page/Auth/Login/index.jsx
+++ src/page/Auth/Login/index.jsx
@@ -1,55 +1,32 @@
 import React from "react"
-import {  useNavigate } from "react-router-dom"
-import { useForm } from "react-hook-form"
 import cls from './index.module.css'
 
 const Login = () => {
-  const user = localStorage.getItem('user')
+    const [login, setLogin] = React.useState("")
+    const [password, setPassword] = React.useState("")
 
-  const navigate = useNavigate()
-
-  const {
-    register,
-    handleSubmit,
-    formState: {errors}
-  } = useForm()
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("Login:", login, "Password:", password)
+    }
 
     return (
         <div className={cls.Login}>
-            <input
-                {...register('name', {
-                    required: 'Это поле обязательное',
-                    minLength: {
-                        message: 'Минимум 2 символа',
-                        value: 2
-                    },
-                    maxLength: {
-                        value: 50, 
-                        message: 'Максимум 50 символов'
-                    }
-                })} 
-              placeholder="Login"
-              type="text" 
-            />
-            {
-                errors.name && <p>{errors.name.message}</p>
-            }
-            <input 
-                {...register('password', {
-                    required: 'Это поле обязательное',
-                    minLength: {
-                        message: 'Минимум 6 символа',
-                        value: 2
-                    },
-                    maxLength: {
-                        value: 100, 
-                        message: 'Максимум 100 символов'
-                    }
-                })} 
-                placeholder="********" type="password" 
-            />
-            {errors.name && <p>{errors.password.message}</p>}
-            <button >Войти</button>
+            <form onSubmit={handleSubmit}> 
+                <input 
+                    placeholder="Login" 
+                    type="text" 
+                    value={login} 
+                    onChange={(e) => setLogin(e.target.value)} 
+                />
+                <input 
+                    placeholder="********" 
+                    type="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                />
+                <button type="submit">Войти</button> 
+            </form>
         </div>
     )
 }
